a8f1e0230c51e1edf109b223923a61b78af51643
khanhkhanhlele <namkhanh2172@gmail.com>
2026-03-09 15:41:34 +0700

    update

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ netuid = 124
 
 subtensor = bt.Subtensor("finney")
 
-burn_cost = subtensor.recycle(netuid)
+all_info = {int(s.netuid): s for s in subtensor.get_all_subnets_info()}
+info = all_info.get(netuid)
 
-print("Miner registration cost:", burn_cost)
+print("register_fee_tao:", float(info.burn) if info and info.burn is not None else None)
